a29cf189ee5f56e1d7f1d999259c8c162cffd7c2
Jonathan A. Kimball <jonathan.kimball64@gmail.com>
2019-06-06 08:57:46 -0700

    question array

--- js/app.js
+++ js/app.js
@@ -1,18 +1,39 @@
+'use strict';
 
+var questionAndAnswersList = [
+  ['Question 1?', 'Q1 Reply if Yes.', 'Q1 Reply if No'],
+  ['Question 2?', 'Q2 Reply if Yes.', 'Q2 Reply if No'],
+  ['Question 3?', 'Q3 Reply if Yes.', 'Q3 Reply if No'],
+  ['Question 4?', 'Q4 Reply if Yes.', 'Q4 Reply if No'],
+  ['Question 5?', 'Q5 Reply if Yes.', 'Q5 Reply if No'],
+];
+var yesNoQuestionCount = 5;
+
+// Question Counts (qCount...)
+var qcOnlyJoking = 0;
+var qCountYesNo = 5;
+var totalQuestionCount = qCountYesNo;
+var correctResponseCount = 0;
+
+// Find out who is playing
 var promptWho = prompt('Welcome to my page.  What is your name?');
 console.log('Who: ' + promptWho);
 
-var promptWhat = prompt('Hi, ' + promptWho + ' what do you do for fun? (state with the verb first)');
-console.log('What: ' + promptWhat);
+// In a loop ask each of the Yes/No questions.
+// Within that loop, use a nested loop to validate if response was Y or N.
+var userResponse = '';
+for (var i = 0 ; i < qCountYesNo; i++) {
+  console.log (i + questionAndAnswersList[0,i]);
+  userResponse = '';
+  while ((userResponse === 'Y' || userResponse === 'N') === false) {
+    userResponse = prompt('(Y/n) ' + questionAndAnswersList[0,i]).toUpperCase[0];
+    console.log('Response to Y/n Q' + i + ': ' + userResponse);
+  } 
+};
 
-var confirmYes = confirm('Confirm?');
-console.log(confirmYes)
-/*
-var promptWhere = prompt('Where do you ' + promptWhat + '?');
-console.log('Where: ' + promptWhere);
+// This one's not right
+var fairGame = false;
+prompt('Guess which number I\'m thinking of!');
+prompt('No! Guess again!');
 
-var promptWhen = prompt('When do you usually find the time to ' + promptWhat + ' in ' + promptWhere + '?');
-console.log('When: ' + promptWhen);
-
-alert('Have fun ' + promptWhat + ' in ' + promptWhere + ' next ' + promptWhen + ', ' + promptWho + '!');
-*/
+// for (var guessCount = 4; guessCount > 0 ; ++guessCount) {}
